84cc590ea6b65dff65b7383cd42aa512cb991671
Vicente Cser <vicentecser@gmail.com>
2025-01-28 12:05:55 -0300

    Refactor CoordinatesForm to use event handling for form submission; add fetch functions for meteorology and geography data

--- src/components/CoordinatesForm.jsx
+++ src/components/CoordinatesForm.jsx
@@ -1,18 +1,19 @@
 import CoordinatesContainer from "./CoordinatesContainer";
 
 export default function CoordinatesForm() {
-    function handleSubmit(formData) {
+    function handleSubmit(e) {
+        e.preventDefault();
+        const formData = new FormData(e.target);
         for (const pair of formData.entries()) {
             console.log(pair[0], pair[1]);
         }
     }
 
     return (
-        <form name="coordinates-form" onSubmit={(e) => {
-            e.preventDefault();
-            const formData = new FormData(e.target);
-            handleSubmit(formData);
-        }}>
+        <form
+            name="coordinates-form"
+            onSubmit={handleSubmit}
+        >
             <CoordinatesContainer />
             <button type="submit" data-testid="submit">Submit</button>
         </form>
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -8,3 +8,138 @@ const fetchData = async () => {
 };
 
 export default fetchData;
+
+async function fetchMeteorology(coords) {
+    const lats = coords.map((coord) => coord.lat).join(",");
+    const longs = coords.map((coord) => coord.long).join(",");
+    const response = await fetch(
+        `https://api.open-meteo.com/v1/forecast?latitude=${lats}&longitude=${longs}&current=temperature_2m,relative_humidity_2m,wind_speed_10m,wind_direction_10m`,
+    );
+    const json = await response.json();
+
+    const data = json.map((d) => ({
+        humidity: d.current.relative_humidity_2m,
+        velocity: d.current.wind_speed_10m,
+        temperature: d.current.temperature_2m,
+        direction: d.current.wind_direction_10m,
+    }));
+
+    return data;
+}
+
+async function fetchGeography(coords) {
+    // let response = await fetch(
+    //     `http://chome.inf.udec.cl:8000/slope?lat=${lat}&lon=${long}`,
+    // );
+    // response = await response.json();
+    // const slope = response.slope;
+
+    // // paso 4: obtener modelo de combustible
+    // response = await fetch(
+    //     `http://chome.inf.udec.cl:8000/fuel-model?lat=${lat}&lon=${long}`,
+    // );
+    // response = await response.json();
+    // const fuelModel = response.fuel_model;
+    // const vpl_factor = response.vpl_factor;
+
+    // // paso 5: obtener distancia a la zona poblada mas cercana
+    // response = await fetch(
+    //     `http://chome.inf.udec.cl:8000/nearest-populated-area?lat=${lat}&lon=${long}`,
+    // );
+    // response = await response.json();
+    // const distance = response.distance_meters / 1000;
+
+    const data = coords.map(async (coord) => {
+        const response = await fetch(
+            `http://chome.inf.udec.cl:8000/geography?lat=${coord.lat}&lon=${coordlong}`,
+        );
+        const json = response.json();
+
+        return {
+            slope: json.slope,
+            fuelModel: json.fuel_model,
+            vplFactor: parseFloat(json.vpl_factor),
+            distance: json.distance_meters / 1000,
+            valorXRodal: 3732,
+        };
+    });
+
+    return data;
+}
+
+function fetchResources(coords) {
+    // obtener datos de recursos junto con sus eta a cada foco
+}
+
+function formInputdata(coords) {
+    const meteorology = fetchMeteorology(coords);
+    const geography = fetchGeography(corrds);
+    const resources = fetchResources(coords);
+
+    const currentTime = new Date().toISOString().slice(0, 16);
+    return `${currentTime}
+    ${coords.length}
+    ${
+        coords.map((coord, idx) =>
+            `${coord.lat} ${coord.long} ${meteorology[idx].humidity} ${
+                meteorology[idx].velocity
+            } ${meteorology[idx].direction} ${meteorology[idx].temperature} ${
+                geography[idx].slope
+            } ${geography[idx].vplFactor} ${coord.timestamp} ${
+                geography[idx].valorXRodal
+            } ${geography[idx].fuelModel} ${geography[idx].distance} 0`
+        ).join("\n")
+    }
+    ${resources.length}
+    ${
+        resources.map((resource) =>
+            `${resource.id} ${resource.type} ${resource.hours} ${resource.lat} ${resource.long} ${resource.state} ${resource.assigned} 1 ${
+                resource.etas.join(" ")
+            }`
+        ).join("\n")
+    }
+    176.6666667 176.6666667 176.6666667 530 441.6666667 441.6666667 441.6666667
+    176.6666667 176.6666667 176.6666667 530 441.6666667 441.6666667 441.6666667
+    176.6666667 176.6666667 176.6666667 530 441.6666667 441.6666667 441.6666667
+    176.6666667 176.6666667 176.6666667 530 441.6666667 441.6666667 441.6666667
+    176.6666667 176.6666667 176.6666667 530 441.6666667 441.6666667 441.6666667
+    176.6666667 176.6666667 176.6666667 530 441.6666667 441.6666667 441.6666667
+    176.6666667 176.6666667 176.6666667 530 441.6666667 441.6666667 441.6666667
+    176.6666667 176.6666667 176.6666667 530 441.6666667 441.6666667 441.6666667
+    176.6666667 176.6666667 176.6666667 530 441.6666667 441.6666667 441.6666667
+    176.6666667 176.6666667 176.6666667 530 441.6666667 441.6666667 441.6666667
+    176.6666667 176.6666667 176.6666667 530 441.6666667 441.6666667 441.6666667
+    176.6666667 176.6666667 176.6666667 530 441.6666667 441.6666667 441.6666667
+    176.6666667 176.6666667 176.6666667 530 441.6666667 441.6666667 441.6666667
+    176.6666667 176.6666667 176.6666667 530 441.6666667 441.6666667 441.6666667
+    176.6666667 176.6666667 176.6666667 530 441.6666667 441.6666667 441.6666667
+    176.6666667 176.6666667 176.6666667 530 441.6666667 441.6666667 441.6666667
+    176.6666667 176.6666667 176.6666667 530 441.6666667 441.6666667 441.6666667
+    176.6666667 176.6666667 176.6666667 530 441.6666667 441.6666667 441.6666667
+    176.6666667 176.6666667 176.6666667 530 441.6666667 441.6666667 441.6666667
+    176.6666667 176.6666667 176.6666667 530 441.6666667 441.6666667 441.6666667
+    176.6666667 176.6666667 176.6666667 530 441.6666667 441.6666667 441.6666667
+    176.6666667 176.6666667 176.6666667 530 441.6666667 441.6666667 441.6666667
+    176.6666667 176.6666667 176.6666667 530 441.6666667 441.6666667 441.6666667
+    176.6666667 176.6666667 176.6666667 530 441.6666667 441.6666667 441.6666667
+    176.6666667 176.6666667 176.6666667 530 441.6666667 441.6666667 441.6666667
+    176.6666667 176.6666667 176.6666667 530 441.6666667 441.6666667 441.6666667
+    176.6666667 176.6666667 176.6666667 530 441.6666667 441.6666667 441.6666667
+    176.6666667 176.6666667 176.6666667 530 441.6666667 441.6666667 441.6666667
+    176.6666667 176.6666667 176.6666667 530 441.6666667 441.6666667 441.6666667
+    176.6666667 176.6666667 176.6666667 530 441.6666667 441.6666667 441.6666667
+    176.6666667 176.6666667 176.6666667 530 441.6666667 441.6666667 441.6666667
+    4988 14
+    30 14
+    37 21
+    103 83
+    4204 4204
+    53663 53663
+    4204 4204
+    ${
+        resources.map((resource) =>
+            `${resource.id} ${"1 ".repeat(coords.length).trim()}`
+        ).join("\n")
+    }
+`;
+}
