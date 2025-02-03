949398b127749be1c807c12e92fce7023d02065a
Vicente Cser <vicentecser@gmail.com>
2025-02-03 12:18:17 -0300

    Refactor ResultsSection to display metrics and resources in tables for improved readability

--- src/components/ResultsSection.jsx
+++ src/components/ResultsSection.jsx
@@ -3,33 +3,36 @@ export default function ResultsSection({ results }) {
         <section>
             {results.metrics
                 ? (
-                    <>
-                        <h1>Metrics:</h1>
-                        <p>
-                            Patrimonio perdido: {results
-                                .metrics["PatrimonioUSD Perdido"]}
-                        </p>
-                        <p>
-                            Patrimonio salvado: {results
-                                .metrics["PatrimonioUSD Salvado"]}
-                        </p>
-                        <p>Area quemada: {results.metrics.AreaQuemada}</p>
-                        <p>Perimetro: {results.metrics.Perimetro}</p>
-                        <p>
-                            Tiempo de apagado: {results
-                                .metrics["Tiempo Apagado (h)"]}
-                        </p>
-                        <h1>Recursos:</h1>
-                        <ul>
-                            {results.resources.map((resource) => (
-                                <li key={resource.id}>
-                                    <p>Id: {resource.id}</p>
-                                    <p>Metros de linea: {resource.metroLinea}</p>
-                                    <p>Costo: {resource.costoRecurso}</p>
-                                </li>
+                    <div>
+                        <table>
+                            <tr>
+                                <th>Metrica</th>
+                                <th>Valor</th>
+                            </tr>
+                            {Object.entries(results.metrics).map((
+                                [key, value],
+                            ) => (
+                                <tr key={key}>
+                                    <td>{key}</td>
+                                    <td>{value}</td>
+                                </tr>
                             ))}
-                        </ul>
-                    </>
+                        </table>
+                        <table>
+                            <tr>
+                                <th>Id</th>
+                                <th>Metros de linea</th>
+                                <th>Costo aproximado</th>
+                            </tr>
+                            {results.resources.map((resource) => (
+                                <tr key={resource.id}>
+                                    <td>{resource.id}</td>
+                                    <td>{resource.metroLinea}</td>
+                                    <td>{resource.costoRecurso}</td>
+                                </tr>
+                            ))}
+                        </table>
+                    </div>
                 )
                 : "Aun no hay resultados"}
         </section>
